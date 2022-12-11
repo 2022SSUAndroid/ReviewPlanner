4501eb98faea5f31ac39d12b1f17b51d1f259fec
yejin <kwonyj1022@naver.com>
2022-12-12 06:14:33 +0900

    문제추가 툴바 추가

--- app/src/main/java/com/example/planner/AddProblemActivity.java
+++ app/src/main/java/com/example/planner/AddProblemActivity.java
@@ -1,6 +1,8 @@
 package com.example.planner;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,6 +17,16 @@ public class AddProblemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_problem);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);  //액티비티의 앱바 지정
+
+
+        ActionBar actionBar = getSupportActionBar();
+        //Title text 작성
+        actionBar.setTitle("문제 추가");
+        //뒤로가기 버튼 추가
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         manager = getSupportFragmentManager();
         addProblemFragment = new AddProblemFragment();
