a9c45e7547c34d2a5109707e1cdd19c9c5de15df
k0000k <locker50@naver.com>
2022-12-12 22:51:26 +0900

    FirstTapBar 카테고리명 파이어베이스 연결 완료

--- app/src/main/java/com/example/planner/Fragment2.java
+++ app/src/main/java/com/example/planner/Fragment2.java
@@ -5,10 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
@@ -17,6 +24,8 @@ public class Fragment2 extends Fragment {
     ArrayList<Category> categories;
     ListView customListView;
     private static CustomAdapter customAdapter;
+    DocumentReference docRef;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public Fragment2() {
 
@@ -28,13 +37,32 @@ public class Fragment2 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment2, container, false);
 
-        categories = new ArrayList<>();
-        categories.add(new Category("영어", "3문제"));
-        categories.add(new Category("수학", "1문제"));
+//        DocumentReference docRef = db.collection("user").document(user.getUid());
+        this.docRef = db.collection("user").document("3rKDL4lMxSR7UnWB35GNoyEeI9s2");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    ArrayList categoryReturnNames = (ArrayList) document.get("categories");
+                    ArrayList<String> categoryNames = new ArrayList<>();
 
-        customListView = (ListView) rootView.findViewById(R.id.listView_custom);
-        customAdapter = new CustomAdapter(getContext(), categories);
-        customListView.setAdapter(customAdapter);
+                    categories = new ArrayList<>();
+                    for (Object category : categoryReturnNames) {
+                        String categoryName = category.toString();
+                        categories.add(new Category(categoryName, "1문제/1문제"));
+                    }
+
+                    customListView = (ListView) rootView.findViewById(R.id.listView_custom);
+                    customAdapter = new CustomAdapter(getContext(), categories);
+                    customListView.setAdapter(customAdapter);
+
+                } else {
+                    Toast.makeText(getActivity(), "해당하는 문제가 없습니다", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
 
         return rootView;
     }
